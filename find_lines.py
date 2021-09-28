eac793ee9342074cc7a83f7059efc2a03e3759a8
June Churchill <juneechurchill@gmail.com>
2021-09-28 01:07:34 -0600

    Add files via upload
    Added combine method, added documentation, standardized formatting

--- find_lines.py
+++ find_lines.py
@@ -5,14 +5,14 @@ Created on Wed Sep 15 02:08:23 2021
 @author: junec
 """
 
-##Line signature location algorithm
-##When inputting a spectrum will return a list of line signatures listed with global prominence 
+## Line signature location algorithm
+## When inputting a spectrum will return a list of line signatures listed with estimated widths
 
 import numpy as np
 from scipy.constants import c
 
 
-def find_lines(wave: list, flux: list, fit_length: int, min_width: float, wavelengths_only = False, weights : list = []):
+def find_lines(wave: list, flux: list, fit_length: int, min_width: float, wavelengths_only = False, weights : list = [], com = True):
     """
     Returns 2 two-dimensional lists listing the detected minima, maxima, and estimated line widths for the inputted spectrum
     
@@ -35,6 +35,8 @@ def find_lines(wave: list, flux: list, fit_length: int, min_width: float, wavele
         If True returns maximums and minimums with only wavelengths listed, default : False
     weights : list
         The weights for the weighted linear regression
+    con : boolean
+        If true consolidates overlapping lines
 
     Returns
     -------
@@ -72,8 +74,6 @@ def find_lines(wave: list, flux: list, fit_length: int, min_width: float, wavele
     
         """
         
-        #print("Check width successfully called")
-        
         index = i
         left_width = 0
         right_width = 0
@@ -91,9 +91,12 @@ def find_lines(wave: list, flux: list, fit_length: int, min_width: float, wavele
                 left_slope = ls_line(index, -1)
                 
                 if left_slope < 0:
+                    
                     left_width = i - (index + fit_length/2)
                     break
+                
                 if index == 0: # catches special case where there are no wavelengths on left side
+                
                     left_width = i
             
             index = i
@@ -107,9 +110,12 @@ def find_lines(wave: list, flux: list, fit_length: int, min_width: float, wavele
                 right_slope = ls_line(index, 1)
                 
                 if right_slope > 0:
+                    
                     right_width = (index + fit_length/2) - i
                     break
+                
                 if index == len(flux) - 1: # catches special case where there are no wavelengths on right side
+                
                     right_width = i
                    
         # minimum mode
@@ -141,21 +147,24 @@ def find_lines(wave: list, flux: list, fit_length: int, min_width: float, wavele
                 right_slope = ls_line(index, 1)
                 
                 if right_slope < 0:
+                    
                     right_width = (index + fit_length/2) - i
                     break
+                
                 if index == len(flux) - 1: # catches special case where there are no wavelengths on right side
+                
                     right_width = i
                     
                     
         return left_width, right_width
     
+    
     def ls_line(i : int, direction : int):
         """
         Helper function for check_width, returns the slope of the data on either side of an inputted wavelength
         
-        Calculates the a least squares linear regression of the fluxes on both
-        sides of a given index in order to find the critical points where the 
-        line signature ends
+        Calculates a weighted least squares linear regression of the fluxes on both
+        sides of a given index. Finds the critical points where the line signature ends
     
         Parameters
         ----------
@@ -174,26 +183,91 @@ def find_lines(wave: list, flux: list, fit_length: int, min_width: float, wavele
         X0, y, res = np.zeros(fit_length), np.zeros(fit_length), np.ones(fit_length)
         
         for n in range(fit_length):
+            
             if i - (n+1) > 0 and direction == -1:
+                
                 X0[n] = wave[i - (fit_length - n)]
                 y[n] = flux[i - (fit_length - n)]
+                
                 if np.any(weights) and len(weights) == len(wave):
+                    
                    res[n] = weights[i - (fit_length - n)]
             
             if i + (n+1) < len(flux) and direction == 1:
+                
                 X0[n] = wave[i + n]
                 y[n] = flux[i + n]
+                
                 if np.any(weights) and len(weights) == len(wave):
+                    
                     res[n] = weights[i + n]
                     
         C = np.diag(res**2)
         
         #MLE least squares weighted linear regression
+        
         X = np.c_[np.ones(X0.shape),X0]
         
         w_wlr = np.linalg.pinv(X.T @ np.linalg.pinv(C) @ X) @ (X.T @ np.linalg.pinv(C) @ y)
         
         return w_wlr[1]
+    
+    
+    def combine(lines : list, maxmin : int, i = 0):
+        """
+        Combines overlapping minima and maxima
+        
+        Parameters
+        ----------
+        lines : list
+            The minima or maxima that need to be checked for overlapping signatures
+        maxmin : int
+            Determines the mode, 1 = max, -1 = min
+        
+        Returns
+        -------
+        lines : list
+            The combined signatures
+        """
+
+        if len(lines) > 1 and i < len(lines) - 1:
+            
+                ## if the bounds of one line overlap the center of the other and vice versa they are overlapping
+                ## combines overlapping lines into one signature
+                ## sets new center line of combined signature to be the local extrema of the two combining lines
+                ## cleans up maximums and minimums from noisy data
+                
+                if (lines[i][0] + lines[i][2] >= lines[i+1][0] and lines[i+1][0] - lines[i+1][1] <= lines[i][0]): 
+                    
+                    # checking minimums
+                    
+                    if maxmin == -1:
+                        
+                        lines[i+1] = (lines[i][0], lines[i][1], lines[i+1][0]-lines[i][0]+lines[i+1][2],
+                                      lines[i][3], lines[i][4], lines[i+1][3]-lines[i][3]+lines[i+1][5]
+                                      ) if flux[lines[i][3]] <= flux[lines[i+1][3]] else ( # finds local minimum of two lines
+                                          lines[i+1][0], lines[i+1][0]-lines[i][0]+lines[i][1], lines[i+1][2],
+                                          lines[i+1][3], lines[i+1][3]-lines[i][3]+lines[i][4], lines[i+1][5])
+                    
+                    # checking maximums 
+                                          
+                    if maxmin == 1:    
+                        
+                        lines[i+1] = (lines[i][0], lines[i][1], lines[i+1][0]-lines[i][0]+lines[i+1][2],
+                                      lines[i][3], lines[i][4], lines[i+1][3]-lines[i][3]+lines[i+1][5]
+                                      ) if flux[lines[i][3]] >= flux[lines[i+1][3]] else ( # finds local maximum of two lines
+                                          lines[i+1][0], lines[i+1][0]-lines[i][0]+lines[i][1], lines[i+1][2],
+                                          lines[i+1][3], lines[i+1][3]-lines[i][3]+lines[i][4], lines[i+1][5])
+                    
+                if lines[i+1][0] == lines[i][0]: # checks if two lines were just combined
+                
+                    del(lines[i]) # deletes duplicate line
+                    lines = combine(lines, maxmin, i) # checks if any other lines lie within the new combined line
+                    
+                else: 
+                    lines = combine(lines, maxmin, i + 1) # if no other lines overlapping on current index moves to next line
+        
+        return lines
 
     
     maximums = [[0,0,0,0,0,0]]
@@ -251,14 +325,22 @@ def find_lines(wave: list, flux: list, fit_length: int, min_width: float, wavele
                        
                       minimums.append([wave[index], left*λ, right*λ, index, left, right])
     
+    # combines overlapping lines
+    
+    if com:
+        
+        maximums = combine(maximums, 1)
+        minimums = combine(minimums, -1)
+    
     if wavelengths_only:
+        
         maximums = [a[0] for a in maximums]
         minimums = [a[0] for a in minimums]
     
     return maximums, minimums
  
 
-def vel(wave : list, flux : list, fit_length : int, min_width : float, rest_wavelength : float, wavelengths_only = False, weights=None):
+def vel(wave : list, flux : list, fit_length : int, min_width : float, rest_wavelength : float, wavelengths_only = False, weights = None, com = True):
     """
     Finds the relative velocities of all line signatures around an inputted rest wavelength
 
@@ -292,14 +374,16 @@ def vel(wave : list, flux : list, fit_length : int, min_width : float, rest_wave
 
     """
     
-    maximums, minimums = find_lines(wave, flux, fit_length, min_width, wavelengths_only, weights)
+    maximums, minimums = find_lines(wave, flux, fit_length, min_width, wavelengths_only, weights, com)
     
-    def delta(wavelength : float):
+    def delta(wavelength : float): # calculates line velocity from rest_wavelength
         return ((c/1000) * (wavelength - rest_wavelength) / rest_wavelength)
     
     if wavelengths_only:
+        
         maximums = [[a, delta(a)] for a in maximums if np.abs(delta(a)) < 30000]
         minimums = [[a, delta(a)] for a in minimums if np.abs(delta(a)) < 30000]
+        
         return maximums, minimums
     
     maximums = [a + [delta(a[0])] for a in maximums if np.abs(delta(a[0])) < 30000]
@@ -307,16 +391,38 @@ def vel(wave : list, flux : list, fit_length : int, min_width : float, rest_wave
             
     return maximums, minimums
 
+
 def validate_list(lists):
+    """
+    Checks if user input is of a valid format to be analyzed
+    
+    Parameters
+    ----------
+    lists : list
+        User inputted data either wavelengths or flux
+        
+    Returns
+    -------
+    TypeError
+        If data is not a list or is empty list
+    ValueError
+        If data is only 1 entry long
+    True
+        If data is valid for analysis
+    """
     
     if not isinstance(lists, list):
+        
         return TypeError
     
     for n in lists:
+        
         if not isinstance(n, (float, int)):
+            
             return TypeError
         
     if len(lists) == 1:
+        
         return ValueError
         
     return True
